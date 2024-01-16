324e93461e498187cc7c7ac25292e1f4e92180a8
Dandraghas <dandraghas@gmail.com>
2024-01-16 22:25:39 +0200

    Add spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,25 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.diffplug.spotless") version "6.22.0"
+}
+
+spotless {
+    format("misc") {
+        target("**/*.gradle", "**/*.md", "**/.gitignore")
+
+        indentWithTabs()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+
+    kotlin {
+        ktfmt("0.46").kotlinlangStyle()
+
+        target("**/*.kt")
+        trimTrailingWhitespace()
+        indentWithTabs()
+        endWithNewline()
+    }
 }
